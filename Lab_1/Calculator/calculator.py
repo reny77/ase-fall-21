d36428ae5c15a606ae7c93c83e4d964311f6ded0
reny <r.eschini@inera.it>
2020-09-19 13:40:37 +0200

    fix bug and run unit tests again

--- Lab_1/Calculator/calculator.py
+++ Lab_1/Calculator/calculator.py
@@ -1,16 +1,23 @@
 #calculator.py
 
 def sum(m,n):
-    x = m
-    for _ in range(n):
-        x = x+1
-    return x
+    times = abs(n) # number of iterations
+    inc = 1 if n > 0 else -1 # check if I need to sum or sub (if n is negative)
+    for i in range(times): # loop 'times' for increment
+        m += inc # increment
+    return m # return result
 
 def divide(m,n):
-    x = m
-    for _ in range(n):
-        x = x-n
-    return x
+    if n == 0: raise ZeroDivisionError    
+    times = 0 # result
+    sign = 1 if ((n > 0) == (m > 0)) else -1 # if the sign is not equal, the sign is negative
+    tmpM = abs(m) # ignore sign
+    tmpN = abs(n) # ignore sign
+    tmpM -= tmpN # first subtract tmpN
+    while tmpM >= 0: # iterate while the tmpM var is positive, when is equal or less zero, exit while       
+        times += 1 # accumulate which time the tmpN is subtracted from tmpM
+        tmpM -= tmpN # subtract tmpN
+    return times * sign # return result with sign
 
 def main():
     print(f"Result is: {sum(10,3)}")
